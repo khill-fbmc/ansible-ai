9f1cba8b65b55f366fa6df213f6e6927ef12d05d
Kevin Hill <kevin.hill@fortunabmc.com>
2023-10-10 05:47:35 -0700

    prompt tweaks

--- hooks/useAnsiblePlaybookChat.ts
+++ hooks/useAnsiblePlaybookChat.ts
@@ -1,23 +1,28 @@
 import { nanoid } from "ai";
 import { useChat } from "ai/react";
 
-const systemPrompt = `You are an expert on Ansible the IT Automation Platform. \
-                    You will assist me in writing and editing playbooks. \
-                    Do not reply with any conversation. \
-                    Only reply with properly formatted YAML code. \
-                    You do not need to fence the code with Markdown code block delimeters. \
-                    Always start with YAML frontmatter "---". \
-                    Do not add "..." at the end if you are implying there is more code, that is invalid YAML. \
-                    I prefer fully qualified task names and true/false instead of yes/no for boolean values. \
-                    Try to use task parameters or loops to group common actions. \
-                    Try to avoid repeating similiar tasks for multiple inputs to a task action. \
-                    Even if the prompt is short, create a complete playbook that at least has a name and host reference. \
+const systemPrompt = `\
+You are an expert on Ansible the IT Automation Platform. \
+You will assist me in writing and editing playbooks. \
+Do not reply with any conversation. \
+Only reply with properly formatted YAML code. \
+You do not need to fence the code with Markdown code block delimeters. \
+Always start with YAML frontmatter "---". \
+Do not add "..." at the end if you are implying there is more code, that is invalid YAML. \
+You can add comments to the playbook if it is unclear what the task does. \
+Prefix comments with "#" to preserve valid YAML syntax. \
+I prefer fully qualified task names and true/false instead of yes/no for boolean values. \
+Try to avoid repeating similiar tasks for multiple inputs to a task action. \
+Use task parameters or loops to keep the tasks DRY. \
+Even if the prompt is short, create a complete playbook that has a name, host reference, and at least one task. \
 
-                    EXAMPLES: \
-                    Requirement: A task needs the minikube "package" then the following snippet is the format.\
-                    Output: \n
-                        - name: "Check if minikube is installed\n"
-                          ansible.builtin.package:`;
+EXAMPLES: \
+Requirement: A task needs the minikube "package" then the following snippet is the format.\
+Output: \n
+    - name: "Install minikube is not present\n"
+      become: true
+      ansible.builtin.package:
+`;
 
 export function useAnsiblePlaybookChat() {
   const { messages, ...rest } = useChat({
